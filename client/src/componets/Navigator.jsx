9554e84d27a0b4e84af6a976298be30a9d4b6e99
iUsup <yusub1988@gmail.com>
2023-10-14 17:50:36 +0300

    Add: Navbar box and buttons

--- client/src/componets/Navigator.jsx
+++ client/src/componets/Navigator.jsx
@@ -1,18 +1,30 @@
-import { Link } from 'react-router-dom';
-import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+
+import './navigator.css';
+
 
 
 const Navbar = () => {
  
     return (
         <header>
-            <div className="container">
-                <Link to="/">
-                    <Typography variant="h5" gutterBottom>
-                        h5. Heading
-                    </Typography>
-                </Link>
-            </div>
+            <Box
+                sx={{
+                display: 'flex',
+                justifyContent: 'space-evenly',
+                p: 1,
+                m: 1,
+                bgcolor: 'background.paper',
+                borderRadius: 1,
+                }}
+            >
+                <Button variant="text">Text</Button>
+                <Button variant="contained">Contained</Button>
+                <Button variant="outlined">Outlined</Button>
+            </Box>
+            
         </header>
     )
 }
