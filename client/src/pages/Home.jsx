5cc8c1b120986d92f20ac3552299b77fe68d7743
iUsup <yusub1988@gmail.com>
2023-11-19 16:55:01 +0300

    Add: Text to homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -22,39 +22,15 @@ const Home = () => {
     }, [])
 
     return (
-        
-        
         <Box sx={{ width: '100%' }}>
-            
-            {homePageText ? 
-            <Typography variant="h3" align='center' gutterBottom> {homePageText.title} </Typography>: 
+            {homePageText 
+            ? 
+            <>
+                <Typography variant="h3" align='center' gutterBottom> {homePageText.title} </Typography>
+                <Typography variant="subtitle1" align='center'  gutterBottom> {homePageText.text} </Typography>
+            </>
+            : 
             <Typography variant="h3" align='center' gutterBottom> Loading... </Typography>}
-                
-        {/* //     
-            
-        //     <Typography variant="subtitle1" gutterBottom>
-        //         {homePageText && .text}
-        //     </Typography>
-        //     <Typography variant="subtitle2" gutterBottom>
-        //         {homePageText.text}
-        //     </Typography>
-        //     <Typography variant="body1" gutterBottom>
-        //         {homePageText.text}
-        //     </Typography>
-        //     <Typography variant="body2" gutterBottom>
-        //         {homePageText.text}
-        //     </Typography>
-        //     <Typography variant="button" display="block" gutterBottom>
-        //         {homePageText.text}
-        //     </Typography>
-        //     <Typography variant="caption" display="block" gutterBottom>
-        //         {homePageText.text}
-        //     </Typography>
-        //     <Typography variant="overline" display="block" gutterBottom>
-        //         {homePageText.text}
-        //     </Typography> 
-        // </Box>   */}
-       
         </Box>
     )
 }
